394212e9cfae70092948e1973cc79a2e2047126f
AnnikaLindberg <annika.edit.lindberg@gmail.com>
2024-04-03 09:33:52 +0200

    resurser update

--- src/components/Resources-data.js
+++ src/components/Resources-data.js
@@ -1,6 +1,6 @@
 export const resources = {
   podcasts: [
-  /*   {
+    /*   {
       id: 1,
       title: "Förlossningskanalen",
       source: "Förlossningskanalen",
@@ -12,99 +12,117 @@ export const resources = {
 subject: "Förlossningsrädsla"    }, */
     {
       id: 2,
-      title: "BABYBABYPODDEN",
-      source: "Lisen Forsberg, skapare av bloggen BabyBaby",
-      podcastName: "BABYBABYPODDEN",
-      episodeName: "",
-      link: "https://babybabypodden.se/",
-      description: "En podd om livet med små barn",
-      type: "Podcast",
-subject: "Föräldraskap"    },
+      title: 'BABYBABYPODDEN',
+      source: 'Lisen Forsberg, skapare av bloggen BabyBaby',
+      podcastName: 'BABYBABYPODDEN',
+      episodeName: '',
+      link: 'https://babybabypodden.se/',
+      description: 'En podd om livet med små barn',
+      type: 'Podcast',
+      subject: 'Föräldraskap',
+    },
   ],
 
   blogPosts: [
     {
       id: 1,
-      title: "DET ÄR DU SOM BESTÄMMER OM IGÅNGSÄTTNING",
-      source: "Föda med stöd",
-      blogName: "Föda med stöd",
-      link: "https://xn--fdamedstd-07ah.se/det-ar-du-som-bestammer-om-igangsattning/",
-      description: "Extern länk till en artikel av Eva Rydahl, är (dansk) jordemoder, Ph.d. och forskare. Översatt av Märta Cullhed Engblom",
-      type: "Artikel",
-subject: ""    },
+      title: 'DET ÄR DU SOM BESTÄMMER OM IGÅNGSÄTTNING',
+      source: 'Föda med stöd',
+      blogName: 'Föda med stöd',
+      link: 'https://xn--fdamedstd-07ah.se/det-ar-du-som-bestammer-om-igangsattning/',
+      description:
+        'Extern länk till en artikel av Eva Rydahl, är (dansk) jordemoder, Ph.d. och forskare. Översatt av Märta Cullhed Engblom',
+      type: 'Artikel',
+      subject: '',
+    },
     // Add more blog posts here
   ],
-  "Föda barn som HBTQIA+": [
+  'Föda barn som HBTQIA+': [
     {
       id: 1,
-      title: "Att bli och vara förälder som hbtqia+ person",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/foraldrastodjande-material/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-subject: "hbtqia+ föräldrastödjande"   },
+      title: 'Att bli och vara förälder som hbtqia+ person',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/foraldrastodjande-material/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
+    },
     {
       id: 2,
-      title: "Förlossningsförberedelser för hbtqia+ -föräldrar",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/forlossningsforberedelser/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-subject: "Förlossningsförberededande för hbtqia+"  },
+      title: 'Förlossningsförberedelser för hbtqia+ -föräldrar',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/forlossningsforberedelser/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
+    },
     {
       id: 3,
-      title: "Att bära barn som trans",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/trans-och-foraldraskap/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-subject: "Bära barn som transperson"
+      title: 'Att bära barn som trans',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/trans-och-foraldraskap/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
     },
     {
       id: 4,
-      title: "Amning och flaskmatning för hbtqia+ -föräldrar",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-      subject: "hbtqia+ A+ och amning"    },
-
+      title: 'Amning och flaskmatning för hbtqia+ -föräldrar',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
+    },
     {
       id: 5,
-      title: "Stöd till hbtqia+ -föräldrar som ammar",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/stod-till-hbtqia+ -personer-som-ammar/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-      subject: "hbtqia+ och amning"    },
+      title: 'Stöd till hbtqia+ -föräldrar som ammar',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/stod-till-hbtqia+ -personer-som-ammar/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
+    },
     {
       id: 6,
-      title: "Guide till samamning och igångsatt amning",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/guide-till-samamning-och-igangsatt-amning/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-      subject: "hbtqia+ och amning"
+      title: 'Guide till samamning och igångsatt amning',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/guide-till-samamning-och-igangsatt-amning/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
     },
     {
       id: 7,
-      title: "Att möta vården som hbtqia+ -förälder",
-      source: "RFSL",
-      organization: "Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)",
-      link: "https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/att-mota-varden/",
-      description: "Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.",
-      type: "Artikel",
-      
+      title: 'Att möta vården som hbtqia+ -förälder',
+      source: 'RFSL',
+      organization:
+        'Riksförbundet för homosexuellas, bisexuellas, transpersoners och queeras rättigheter (RFSL)',
+      link: 'https://www.rfsl.se/verksamhet/foralder/att-bli-och-vara-foralder/att-mota-varden/',
+      description:
+        'Extern länk till RFSL:s föräldrastödjande material för hbtqia+ -personer.',
+      type: 'Artikel',
+      subject: 'HBTQIA',
     },
     // Add more hbtqia+  materials here
   ],
   // Add more categories and resources as needed
 };
-
-
--- src/pages/TipsOchFördjupning.js
+++ src/pages/TipsOchFördjupning.js
@@ -1,14 +1,22 @@
-
 import React from 'react';
-import { Typography, Grid, Card, CardActionArea, CardContent, Link, Box } from '@mui/material';
+import {
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardContent,
+  Link,
+  Box,
+} from '@mui/material';
 import PageWrapper from '../components/PageWrapper';
 import PageSpecificHero from '../components/PageSpecificHero';
 import { Helmet } from 'react-helmet';
 import { resources } from '../components/Resources-data';
 import MyBreadcrumbs from '../components/BreadCrumbs';
-import WebAssetIcon from '@mui/icons-material/WebAsset';  
+import WebAssetIcon from '@mui/icons-material/WebAsset';
 import PodcastsIcon from '@mui/icons-material/Podcasts';
-
+import LgbtIcon from '../assets/lgbtq.svg';
+import styled from 'styled-components';
 
 const TipsOchFördjupning = () => {
   const getCategoryTitle = (categoryKey) => {
@@ -26,77 +34,153 @@ const TipsOchFördjupning = () => {
     if (resource.type === 'Podcast') {
       Icon = PodcastsIcon;
     }
-    return <Icon aria-hidden="true" />;
+    return <Icon aria-hidden='true' />;
   };
 
+  const LgbtqIcon = styled.img`
+    margin: 1rem;
+    width: 50px;
+    height: 50px;
+  `;
   return (
     <PageWrapper>
       {/* Helmet and Breadcrumbs remain unchanged */}
       <Helmet>
         {/* SEO Meta Tags */}
         <title>Tips och fördjupning</title>
-        <link rel="canonical" href="https://www.doulaskapet.se/TipsOchFördjupning" />
-        <meta name="description" content="Hitta användbara resurser som podcasts, blogginlägg och informationsmaterial. Bland annat riktat till dig som är hbtqia+ och blivande förälder." />
+        <link
+          rel='canonical'
+          href='https://www.doulaskapet.se/TipsOchFördjupning'
+        />
+        <meta
+          name='description'
+          content='Hitta användbara resurser som podcasts, blogginlägg och informationsmaterial. Bland annat riktat till dig som är hbtqia+ och blivande förälder.'
+        />
       </Helmet>
-      <MyBreadcrumbs currentPage="Tips och Fördjupning" />
-      <PageSpecificHero title="Tips och Fördjupning" subtitle="Vi i DoulaSkapet älskar att dela med oss av artiklar, blogginlägg och tips på poddar om födande, graviditet och att vara förälder. Här försöker vi ge våra bästa lyssnings- och lästips till dig som väntar barn." />
-      <Typography variant="body1" sx={{ mb: 3 }}>
-        Det är lätt att som blivande förälder bli överväldigad av all information som finns tillgänglig. I DoulaSKapets kunskapsbank vill vil tipsa om resurser som vi tycker är speciellt användbara för er som väntar barn. Till exempel blogginlägg som vi gillar, podcastavsnitt som berör vissa ämnen samt värdefulla resurser till dig som är HBTQIA+.
+      <MyBreadcrumbs currentPage='Tips och Fördjupning' />
+      <PageSpecificHero
+        title='Tips och Fördjupning'
+        subtitle='Vi i DoulaSkapet älskar att dela med oss av artiklar, blogginlägg och tips på poddar om födande, graviditet och att vara förälder. Här försöker vi ge våra bästa lyssnings- och lästips till dig som väntar barn.'
+      />
+      <Typography
+        variant='body1'
+        sx={{ mb: 3 }}
+      >
+        Det är lätt att som blivande förälder bli överväldigad av all
+        information som finns tillgänglig. I DoulaSKapets kunskapsbank vill vil
+        tipsa om resurser som vi tycker är speciellt användbara för er som
+        väntar barn. Till exempel blogginlägg som vi gillar, podcastavsnitt som
+        berör vissa ämnen samt värdefulla resurser till dig som är HBTQIA+.
       </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        Denna sida är under uppbyggnad, och vi tar hemskt gärna emot tips på saker att dela här. Har du något tips? Välkommen att <Link href="/Kontakt">skriva till oss!</Link>
+      <Typography
+        variant='body1'
+        sx={{ mb: 2 }}
+      >
+        Denna sida är under uppbyggnad, och vi tar hemskt gärna emot tips på
+        saker att dela här. Har du något tips? Välkommen att{' '}
+        <Link href='/Kontakt'>skriva till oss!</Link>
       </Typography>
 
-      <Grid container spacing={4} justifyContent="center">
-        {Object.keys(resources).map(category => (
-          <React.Fragment key={category}> {/* Use React.Fragment to wrap each category */}
-            <Grid item xs={12}>
-              <Typography variant="h3" sx={{ mb: 2 }}>
+      <Grid
+        container
+        spacing={4}
+        justifyContent='center'
+      >
+        {Object.keys(resources).map((category) => (
+          <React.Fragment key={category}>
+            {' '}
+            {/* Use React.Fragment to wrap each category */}
+            <Grid
+              item
+              xs={12}
+            >
+              <Typography
+                variant='h3'
+                sx={{ mb: 2 }}
+              >
                 {getCategoryTitle(category)}
               </Typography>
             </Grid>
-            <Grid item xs={12}>
-              <Grid container spacing={2}>
-                {resources[category].map(resource => (
-                 <Grid item xs={12} sm={6} md={4} lg={3} key={resource.id}>
-                 <Card
-                   sx={{
-                     transition: '0.3s', // Smooth transition for hover effect
-                     ':hover': {
-                       boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)', // Example hover effect
-                     }
-                   }}
-                 >
-<CardActionArea
-      component="a" 
-      href={resource.link} 
-      target="_blank" 
-      rel="noopener noreferrer"
-      sx={{
-        outline: 'none', // Removes default focus outline
-        ':focus-visible': { // CSS pseudo-class for focus visible to keyboard users only
-          boxShadow: '0 0 0 3px rgba(25, 118, 210, 0.5)', // Example focus indicator
-        },
-      }}
-    >
-<CardContent sx={{ display: 'block' }}>
-  <Typography gutterBottom variant="h6" component="h3" sx={{ display: 'block' }}>
-    {resource.title}
-  </Typography>
-  {/* Ensuring the icon also behaves like a block element */}
-  <Box sx={{ display: 'block', my: 1 }}>{renderResourceType(resource)}</Box>
-  <Typography variant="paragraph" color="text.secondary" sx={{ display: 'block' }}>
-    {resource.description}
-  </Typography>
-  <Typography variant="" color="text.secondary" sx={{ display: 'block' }}>
-    {resource.subject}
-  </Typography>
-  <Typography variant="paragraph" color="text.secondary" sx={{ display: 'block' }}>
-    {resource.organization}
-  </Typography>
-</CardContent>
-
-    </CardActionArea>
+            <Grid
+              item
+              xs={12}
+            >
+              <Grid
+                container
+                spacing={2}
+              >
+                {resources[category].map((resource) => (
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={3}
+                    key={resource.id}
+                  >
+                    <Card
+                      sx={{
+                        transition: '0.3s', // Smooth transition for hover effect
+                        ':hover': {
+                          boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)', // Example hover effect
+                        },
+                      }}
+                    >
+                      <CardActionArea
+                        component='a'
+                        href={resource.link}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        sx={{
+                          outline: 'none',
+                          ':focus-visible': {
+                            boxShadow: '0 0 0 3px rgba(25, 118, 210, 0.5)', // Example focus indicator
+                          },
+                        }}
+                      >
+                        {' '}
+                        {resource.subject === 'HBTQIA' && (
+                          <LgbtqIcon
+                            src={LgbtIcon}
+                            alt='LGBT Icon'
+                          />
+                        )}
+                        <CardContent sx={{ display: 'block' }}>
+                          <Typography
+                            gutterBottom
+                            variant='h6'
+                            component='h3'
+                            sx={{ display: 'block' }}
+                          >
+                            {resource.title}
+                          </Typography>
+                          {/* Ensuring the icon also behaves like a block element */}
+                          <Box sx={{ display: 'block', my: 1 }}>
+                            {renderResourceType(resource)}
+                          </Box>
+                          <Typography
+                            variant='paragraph'
+                            color='text.secondary'
+                            sx={{ display: 'block' }}
+                          >
+                            {resource.description}
+                          </Typography>
+                          <Typography
+                            variant=''
+                            color='text.secondary'
+                            sx={{ display: 'block' }}
+                          >
+                            {resource.subject}
+                          </Typography>
+                          <Typography
+                            variant='paragraph'
+                            color='text.secondary'
+                            sx={{ display: 'block' }}
+                          >
+                            {resource.organization}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
                     </Card>
                   </Grid>
                 ))}
